21d6972c04473cf97aeb9818480a3f9ef372b93d
kazumix <kazumix255@hotmail.com>
2015-06-30 18:33:35 +0900

    Add PerfMon
    Add PerfMon class, Invariant TSC support

--- StopWatch/StopWatch.cpp
+++ StopWatch/StopWatch.cpp
@@ -8,21 +8,19 @@
 *
 *
 \*****************************************************************************/
-#include "StopWatch.hpp"
-#include <rt.h>
+#include <algorithm>
+#include "StopWatch.h"
 
-#ifdef _MNCL_RSL_USE
-namespace Mncl{		/// top of namespace
-#endif
 
 StopWatch::StopWatch(void)
 :m_Start(0),
 m_Stop(0),
 m_Diff(0),
-m_NowMeasuring(false),
 m_CPUspeed(0)
 {
-	m_CPUspeed	= GetCPUSpeed();
+	m_CPUspeed			= GetCPUSpeed();
+	GetPentiumCounter	= (true == IsInvariantTSCSupport()) ? GetTSCway2 : GetTSCway1;
+//	GetPentiumCounter	= GetTSCway1;
 }
 
 // copy constructor
@@ -31,54 +29,60 @@ StopWatch::StopWatch( StopWatch& original )
 	m_Start	= original.m_Start;
 	m_Stop	= original.m_Stop;
 	m_Diff	= original.m_Diff;
-	m_NowMeasuring	= original.m_NowMeasuring;
 	m_CPUspeed		= original.m_CPUspeed;
+	GetPentiumCounter	= (true == IsInvariantTSCSupport()) ? GetTSCway2 : GetTSCway1;
 }
 
 StopWatch::~StopWatch(void)
 {
 }
 
+bool StopWatch::IsInvariantTSCSupport(void)
+{
+	DWORD		dwEdx;
+	_asm{
+		mov			eax ,80000007H
+		cpuid
+		mov			dword ptr [dwEdx] ,edx
+	}
+	if( dwEdx & 0x00000100 )
+		return true;
+	else
+		return false;
+}
+
 // start the stopwatch
 bool StopWatch::Start(void)
 {
-	if( ! m_NowMeasuring )
-	{
-		// start watch
-		m_Start			= GetPentiumCounter();
-		m_NowMeasuring	= true;
-
-		return true;
-	}
-	else
-		return false;
+	// start watch
+	m_Start			= GetPentiumCounter();
+	return true;
 }
 
 // stop the stopwatch
 QWORD StopWatch::Stop(void)
 {
-	if( m_NowMeasuring )
-	{
-		// stop watch
-		m_Stop	= GetPentiumCounter();
-		m_NowMeasuring	= false;
+	// stop watch
+	m_Stop	= GetPentiumCounter();
 
-		// calc difference time
-		if( m_Stop < m_Start )
-		{
-			return m_Diff = QWORD(_ULLONG_MAX - m_Start) + m_Stop;
-		}
-		else
-			return m_Diff = m_Stop - m_Start;
+	// calc difference time
+	if( m_Stop < m_Start )
+	{
+		return m_Diff = QWORD(_ULLONG_MAX - m_Start) + m_Stop;
 	}
 	else
-		return (QWORD)0;
+		return m_Diff = m_Stop - m_Start;
 }
 
 // read tsc
-QWORD StopWatch::GetPentiumCounter(void)
+QWORD StopWatch::GetTSCway1(void)
 {
 	_asm	rdtsc;
+}
+
+QWORD StopWatch::GetTSCway2(void)
+{
+	_asm	rdtscp;		// serialized RDTSC
 }
 
 // get CPU speed ( Hz )
@@ -119,16 +123,6 @@ double StopWatch::GetSec(void)
 	return (double)m_Diff * (1.0 / GetCPUSpeed());
 }
 
-QWORD GetRaw(void)
-{
-	return m_Diff;
-}
-
-
-#ifdef _MNCL_RSL_USE
-}		/// end of namespace
-#endif
-
 
 /* sample
 #include "StopWatch.hpp"
@@ -151,3 +145,33 @@ QWORD GetRaw(void)
 	std::cout << "time=" << t1.GetSec() << " s" << std::endl;
 
 */
+
+PerfMon::PerfMon() : StopWatch()
+{
+	Reset();
+}
+PerfMon::~PerfMon()
+{
+}
+void PerfMon::Start()
+{
+	StopWatch::Start();
+}
+void PerfMon::Stop()
+{
+	StopWatch::Stop();
+	float	fMicroSec = (float)GetMicrosec();
+	m_qwWhenMax = (m_fMax < fMicroSec ) ? m_qwCount : m_qwWhenMax;
+	m_fAve = (float)(m_fAve * 0.9) + (float)(fMicroSec * 0.1);
+	m_fMax = std::max( m_fMax ,fMicroSec );
+	m_fMin = std::min( m_fMin ,fMicroSec );
+	m_qwCount++;
+}
+void PerfMon::Reset()
+{
+	m_fAve	= 0;
+	m_fMax	= _RWSTD_FLT_MIN;
+	m_fMin	= _RWSTD_FLT_MAX;
+	m_qwCount	= 0;
+	m_qwWhenMax	= 0;
+}
---
+++ StopWatch/StopWatch.h
@@ -0,0 +1,86 @@
+/*****************************************************************************
+*
+* FILE NAME:		StopWatch.hpp
+*
+*		INtime Pentium counter helper class
+*
+*		programed by Kazumix 2012
+*
+*
+*	// copy constructor
+*	StopWatch(StopWatch&);
+*
+*	// stopwatch control
+*	bool Start(void);			// start watch
+*	QWORD Stop(void);			// stop watch
+*
+*	// CPU power
+*	DWORD GetCPUSpeed(void);	// 
+*
+*	// get time value
+*	double GetNanosec(void);	// return nsec value 
+*	double GetMicrosec(void);	// return usec value 
+*	double GetMillisec(void);	// return msec value 
+*	double GetSec(void);		// return sec value 
+*
+\*****************************************************************************/
+#pragma once
+
+
+#ifdef _MICL_RSL_USE
+#include "micl.h"
+#else
+#define	micl_API		// no import
+#endif
+
+
+#include <rt.h>
+
+
+class micl_API StopWatch
+{
+public:
+	StopWatch(void);
+	StopWatch(StopWatch&);
+	~StopWatch(void);
+public:
+	QWORD		m_Start;
+	QWORD		m_Stop;
+	QWORD		m_Diff;
+	DWORD		m_CPUspeed;		// Hz
+public:
+	bool Start(void);			// start watch
+	QWORD Stop(void);			// stop watch
+	DWORD GetCPUSpeed(void);	// 
+
+	double GetNanosec(void);
+	double GetMicrosec(void);
+	double GetMillisec(void);
+	double GetSec(void);
+private:
+	QWORD (*GetPentiumCounter)(void);
+	static QWORD GetTSCway1(void);
+	static QWORD GetTSCway2(void);
+	bool IsInvariantTSCSupport(void);
+};
+
+
+// performance measuring class
+class PerfMon : StopWatch
+{
+public:
+	PerfMon();
+	~PerfMon();
+public:
+	float		m_fMax;			// 最長時間(us)
+	float		m_fMin;			// 最短時間(us)
+	float		m_fAve;			// 平均時間(us)
+	QWORD		m_qwCount;		// トータル計測回数
+	QWORD		m_qwWhenMax;	// 最後に最長時間をマークした際の計測回数
+public:
+	void Start();		// パフォーマンス計測スタート
+	void Stop();		// パフォーマンス計測ストップ
+	void Reset();		// パフォーマンス統計のリセット
+};
+
+
